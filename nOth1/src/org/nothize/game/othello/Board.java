5ba7598a03df46585b87a157b5e0ec4abb4cc426
nothize@gmail.com <nothize@gmail.com@5c303fc6-cddd-11de-91bf-0503b81643b9>
2009-11-29 13:15:05 +0000

    Corrected starting and generalized it.

--- nOth1/src/org/nothize/game/othello/Board.java
+++ nOth1/src/org/nothize/game/othello/Board.java
@@ -1,9 +1,14 @@
 package org.nothize.game.othello;
 
 public class Board {
-	Piece pieces[][] = new Piece[8][8];
-	
-	public Board() {
+	Piece pieces[][];
+
+   	public Board() {
+		this(8, 8);
+	}
+
+	public Board(int w, int h) {
+		pieces = new Piece[h][w];
 		for (int i = 0; i < pieces.length; i++) {
 			for (int j = 0; j < pieces[i].length; j++) {
 				pieces[i][j] = Piece.EMPTY;
@@ -12,12 +17,13 @@ public class Board {
 		
 		starting();
 	}
-
+	
 	private void starting() {
-		pieces[3][3] = Piece.BLACK;
-		pieces[4][4] = Piece.BLACK;
-		pieces[3][4] = Piece.WHITE;
-		pieces[4][3] = Piece.WHITE;
+		int m = pieces.length / 2;
+		pieces[m-1][m-1] = Piece.WHITE;
+		pieces[m-1][m] = Piece.BLACK;
+		pieces[m][m-1] = Piece.BLACK;
+		pieces[m][m] = Piece.WHITE;
 	}
 
 	public Piece[][] getPieces() {
